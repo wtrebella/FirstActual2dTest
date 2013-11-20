3cb876fefe60091f1699015cd526b9834f648215
Whitaker Trebella <wtrebella@gmail.com>
2013-11-20 17:39:10 -0600

    initial real commit

---
+++ Unity/Assets/Scripts/Boulder.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boulder : MonoBehaviour {
+	public AudioClip thudClip;
+
+	protected bool hasLanded = false;
+	protected Boy boy;
+
+	// Use this for initialization
+	void Awake () {
+		StartCoroutine(DropBoulder());
+		boy = GameObject.Find("Boy").GetComponent<Boy>();
+	}
+
+	IEnumerator DropBoulder() {
+		yield return new WaitForSeconds(1);
+
+		rigidbody2D.isKinematic = false;
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (hasLanded) return;
+
+		if (coll.gameObject.tag == "InvisibleWall") {
+			AudioSource.PlayClipAtPoint(thudClip, transform.position);
+			hasLanded = true;
+			AudioSource.PlayClipAtPoint(boy.screamClip, transform.position, 0.1f);
+			GameObject.Find("Music").GetComponent<AudioSource>().Stop();
+			boy.Bleed();
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Unity/Assets/Scripts/Boy.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boy : MonoBehaviour {
+	public AudioClip screamClip;
+	public float maxSpeed;
+	public float speedMultiplier;
+	public float totalSquashTime;
+	public float totalFlipTime;
+	public ParticleSystem deathParticlesPrefab;
+
+	protected ParticleSystem deathParticles;
+	protected Vector3 originalScale;
+	protected float squashStartTime;
+	protected float lastSquashTime;
+	protected Vector3 velocity = Vector3.zero;
+	protected bool isFacingRight = true;
+	protected bool hasBeenHit = false;
+
+	// Use this for initialization
+	void Awake () {
+		originalScale = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((Time.time - squashStartTime > 0.5f) && deathParticles != null) {
+			if (deathParticles.particleCount == 0) {
+				GameObject.Destroy(deathParticles.gameObject);
+				deathParticles = null;
+			}
+		}
+
+		if (hasBeenHit) {
+			if (transform.localScale.y != 0) {
+				transform.localScale = Vector3.Lerp(
+					originalScale,
+					new Vector3(originalScale.x, 0, originalScale.z),
+					(Time.time - squashStartTime) / totalSquashTime);
+
+				if (transform.localScale.y < 0) transform.localScale = new Vector3(transform.localScale.x, 0, transform.localScale.z);
+			}
+		}
+		else {
+			if ((Input.GetKeyDown(KeyCode.LeftArrow) && isFacingRight) || (Input.GetKeyDown(KeyCode.RightArrow) && !isFacingRight)) {
+				Flip();
+			}
+
+			velocity = new Vector3(speedMultiplier * Input.GetAxis("Horizontal"), 0, 0);
+			transform.position = transform.position + velocity * Time.deltaTime;
+		}
+	}
+
+	void Flip() {
+		isFacingRight = !isFacingRight;
+
+		Go.killAllTweensWithTarget(transform);
+		Go.to(transform, totalFlipTime, new GoTweenConfig().vector3XProp("localScale", isFacingRight?1:-1));
+	}
+
+	public void Bleed() {
+		deathParticles = (ParticleSystem)Instantiate(deathParticlesPrefab, transform.position + new Vector3(0, 1f, 0), Quaternion.identity);
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (hasBeenHit) return;
+
+		if (coll.gameObject.tag == "Boulder") {
+			squashStartTime = Time.time;
+			hasBeenHit = true;
+		}
+	}
+}
