215e777a5b7c92d7d378c95b4781269373aa7053
Whitaker Trebella <wtrebella@gmail.com>
2013-11-21 00:14:43 -0600

    messing with godawful animation system

---
+++ Unity/Assets/Scripts/AnimationCreator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationCreator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		AnimationClip clip = new AnimationClip();
+		//AnimationCurve curve = new AnimationCurve(
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Unity/Assets/Scripts/Braid.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Braid : MonoBehaviour {
+	protected Animator animator;
+	protected int walkHash;
+
+	// Use this for initialization
+	void Awake () {
+		animator = GetComponent<Animator>();
+		walkHash = Animator.StringToHash("Base Layer.Braid Walking");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float curSpeed = animator.GetFloat("Walk Speed");
+		animator.SetFloat("Walk Speed", curSpeed + Time.deltaTime);
+	}
+}
